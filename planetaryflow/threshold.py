56cca4eb168a49033c734a78110cc45c5d3f9289
joypauls <joypaulsen3@gmail.com>
2024-01-28 19:42:44 -0600

    first smoke test

--- planetaryflow/threshold.py
+++ planetaryflow/threshold.py
@@ -11,21 +11,21 @@ class Threshold:
 
     def __init__(self, image):
         # TODO force uint8?
-        self.gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        self.gray = cv2.GaussianBlur(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY), (5, 5), 0)
         if self.gray.dtype != np.uint8:
             raise TypeError("Not uint8, something's up")
         self._fit(self.gray)
 
     def _fit(self, img: np.ndarray):
-        self.threshold, self.mask = cv2.threshold(
+        self.value, self.mask = cv2.threshold(
             img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU
         )
         # this is to account for bias in data leading to aggressive threshold
-        self.threshold = int(self.threshold * SHRINK_FACTOR)
+        self.value = np.clip(int(self.value * SHRINK_FACTOR), 0, 255)
 
     def manual(self, threshold: int):
         """Manually override threshold"""
-        self.threshold = threshold
+        self.value = np.clip(threshold, 0, 255)
         _, self.mask = cv2.threshold(self.gray, threshold, 255, cv2.THRESH_BINARY)
 
     def object_size(self):
--- test/test_smoke.py
+++ test/test_smoke.py
@@ -1,5 +1,22 @@
 """Smoke tests mainly for development"""
 
 import numpy as np
+import cv2
 
-from src.threshold import Threshold
+from planetaryflow.threshold import Threshold
+
+jupiter = cv2.imread("./test/data/images/jupiter_stacked.png")
+
+
+def test_threshold():
+    """Threshold class smoke test"""
+
+    # auto method
+    t = Threshold(jupiter)
+    assert t.value > 0 & t.value < 255
+    assert t.object_size() > 50
+
+    # manual method
+    t = Threshold(jupiter)
+    t.manual(100)
+    assert t.value == 100
